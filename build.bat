d0e416d3da3f1248d469f57b9543258959dbe5de
Abathur8bit <Abathur8bit@8bitcoder.com>
2020-12-06 00:51:31 -0500

    Streamlined build process, only one scp call, and the deploy won't happen unless the build was successful.

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
-pscp -r -p src pi@millipede:workspace/chesslrcontroller
-pscp -p README.md pi@millipede:workspace/chesslrcontroller
-pscp -p CMakeLists.txt pi@millipede:workspace/chesslrcontroller
-
-plink -batch pi@millipede "cd workspace/chesslrcontroller && cmake -DCMAKE_BUILD_TYPE=Debug -f CMakeLists.txt && make"
+pscp -r -p src README.md CMakeLists.txt pi@millipede:workspace/chesslrcontroller
 plink -batch pi@chesslr "kill `ps aux | grep chesslrcontroller | grep -v grep | awk -F' ' '{print $2}'`"
-
-pscp pi@millipede:workspace/chesslrcontroller/chesslrcontroller .
-pscp chesslrcontroller pi@chesslr:workspace/chesslrcontroller
-plink -batch pi@chesslr "chmod 755 workspace/chesslrcontroller"
+plink -batch pi@millipede "cd workspace/chesslrcontroller && cmake -DCMAKE_BUILD_TYPE=Debug -f CMakeLists.txt && make && scp chesslrcontroller chesslr:workspace/chesslrcontroller"
